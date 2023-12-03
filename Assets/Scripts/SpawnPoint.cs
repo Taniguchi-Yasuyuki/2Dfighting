a88000956d82e7e097ea9aff73cf62f01c149114
TaniguchiYasuyuki <jelml67879@yahoo.co.jp>
2023-12-03 19:30:27 +0900

    SpawnPointスクリプトの修正途中

--- Assets/Scripts/SpawnPoint.cs
+++ Assets/Scripts/SpawnPoint.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class SpawnPoint : MonoBehaviour
 {
@@ -10,23 +11,33 @@ public class SpawnPoint : MonoBehaviour
     // HadoukenPrefab に Rigidbody2D がアタッチされている前提
     public float hadoukenSpeed = 10f;
 
+    public Transform spawnPoint;
+
     // Update is called once per frame
     void Update()
     {
         // Vが押されたらHadouken（弾）のプレハブが出現する
         if (Input.GetKeyDown(KeyCode.V))
         {
-            GameObject hadouken = Instantiate(HadoukenPrefab, transform.position, Quaternion.identity);
+            GameObject hadouken = Instantiate(HadoukenPrefab, spawnPoint.transform.position, Quaternion.identity);
 
             // プレイヤーの向きを取得
             float playerDirection = transform.localScale.x;
+            Debug.Log(playerDirection);
 
             // 弾の初期速度を設定
             Rigidbody2D hadoukenRB = hadouken.GetComponent<Rigidbody2D>();
             if (hadoukenRB != null)
             {
+                Debug.Log(hadoukenSpeed * playerDirection);
+
                 // プレイヤーの向きに応じて初期速度を設定
                 hadoukenRB.velocity = new Vector2(hadoukenSpeed * playerDirection, 0f);
+
+                //　画像の向きをキャラに合わせる
+                Vector3 temp = hadouken.transform.localScale;
+                temp.x = transform.localScale.x;
+                hadouken.transform.localScale = temp;
             }
         }
     }
